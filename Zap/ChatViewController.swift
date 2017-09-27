bb8e3c9fcb09ce5ace50614d730331abe381e9e2
Rogério Santos <rbs7@brufpe-11.local>
2017-09-27 15:26:00 -0300

    Pedacinho do chat feito

---
+++ Zap/ChatViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ChatViewController.swift
+//  Zap
+//
+//  Created by Rogério Bezerra Santos on 27/09/17.
+//  Copyright © 2017 Zap. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IncomingCell", for: indexPath) as! ChatTableViewCell
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
