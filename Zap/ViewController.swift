d0fb1c781a1c83895ff7cf1e7190c04cbf3405c2
Rogério Santos <rbs7@cin.ufpe.br>
2017-10-01 15:36:37 -0300

    Tentando gravar o tipo de pessoa

--- Zap/ViewController.swift
+++ Zap/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
 
     @IBOutlet var consButton: UIButton!
 
@@ -17,19 +19,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let savedTipoAcesso = self.defaults.object(forKey: "TipoAcesso") as? String
+        print("QUAL SERÁ O TIPO DE PESSOA?")
+        if savedTipoAcesso != nil {
+            print("JÁ SEI O TIPO DE PESSOA")
+            performSegue(withIdentifier: savedTipoAcesso!, sender: self) //POR QUE NAO FUNCIONA
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
     @IBAction func pressConsButton(_ sender: Any) {
+        self.defaults.set("segueCons", forKey: "TipoAcesso")
         performSegue(withIdentifier: "segueCons", sender: self)
     }
     
     @IBAction func pressSelButton(_ sender: Any) {
+        self.defaults.set("segueSel", forKey: "TipoAcesso")
         performSegue(withIdentifier: "segueSel", sender: self)
     }
     
