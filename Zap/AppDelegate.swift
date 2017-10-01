869ada7f0b53d88607d28283203f84987d97d73c
Matheus Herminio de Carvalho <mhc@172-16-3-29.0000.ufpe.br.iosda.org>
2017-10-01 17:06:01 -0300

    integração vendedor e comprador

--- Zap/AppDelegate.swift
+++ Zap/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 import IQKeyboardManagerSwift
-
+import Braintree //adicionando Braintree
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Braintree.setReturnURLScheme("com.zap.zap.payments")
         IQKeyboardManager.sharedManager().enable = true
         return true
     }
